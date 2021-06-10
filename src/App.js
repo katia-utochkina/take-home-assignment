3188e9f09d65821a64f45478d3fc00a0c845224b
Katia <ekaterina.utochkina2@gmail.com>
2021-06-10 10:40:29 +0300

    Call setTextInput with new state object

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 const defaultInput = {
   value: "Here is some example text.",
   textCase: "",
 };
 
-
 function App() {
   const [textInput, setTextInput] = React.useState(defaultInput);
-  const [textOutput, setTextOutput] = React.useState('');
+  const [textOutput, setTextOutput] = React.useState("");
 
-  const handleChange = event => {
-    setTextInput(event.target.value);
+  const handleChange = (event) => {
+    setTextInput({
+      ...textInput,
+      [event.target.name]: event.target.value,
+    });
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setTextOutput('Your formatted text will go here!')
+    setTextOutput("Your formatted text will go here!");
   };
 
   return (
@@ -26,16 +28,15 @@ function App() {
         <h1>Career Lab | Take-Home Assignment</h1>
       </header>
 
-      <main> 
-      <form className="form" onSubmit={handleSubmit}>
+      <main>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>
+              <textarea id="value" name="value" onChange={handleChange} value={textInput.value} required />
+            </label>
+          </div>
 
-      <div className="form-group">
-        <label>
-        <textarea id="value" name="value" onChange={handleChange} value={textInput.value} required />
-        </label>
-      </div>
-
-      <div className="form-group">
+          <div className="form-group">
             <label htmlFor="textCase">Preferred Mode</label>
             <div className="form-field select">
               <select id="textCase" name="textCase" onChange={handleChange} value={textInput.textCase}>
@@ -46,11 +47,9 @@ function App() {
               <span className="focus"></span>
             </div>
           </div>
-            <button type="submit">Submit</button> 
-      </form>
-      <div id="result">
-        {textOutput}
-      </div>
+          <button type="submit">Submit</button>
+        </form>
+        <div id="result">{textOutput}</div>
       </main>
     </div>
   );
